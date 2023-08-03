4a1af6829cf231ffc5b56293d32a69352a2b54b7
Mike Tery <miketery@gmail.com>
2023-08-02 22:32:41 -0400

    starting on splash screen

---
+++ config.js
@@ -0,0 +1,60 @@
+import {
+    env_test, env_dev, 
+    env_debug, env_local,
+} from '@env';
+
+export const TEST = env_test === 'true'
+export const DEV = env_dev === 'true'
+export const DEBUG = env_debug === 'true'
+export const LOCAL = env_local === 'true'
+
+export const SPLASH_ANIMATE_TIME = DEBUG ? 100 : 666
+
+// export const BASE =
+//     LOCAL ? 'http://localhost:8000/api' : 
+//     DEV ? 'https://api.ssi.arxsky.dev/api' : 
+//           'https://api.ssi.arxsky.com/api'
+
+export const ENDPOINTS = {
+    
+}
+
+DEBUG && console.log('DEBUG: ', {
+    TEST: TEST, DEV: DEV, LOCAL: LOCAL, BASE: BASE
+})
+
+export const ROUTES = {
+    TestRoute: 'TestRoute',
+    //
+    SplashRoute: 'SplashRoute',
+    LandingRoute: 'LandingRoute',
+    UnlockRoute: 'UnlockRoute',
+    RegisterRoute: 'RegisterRoute',
+
+    //
+    HomeRoute: 'HomeRoute',
+    MainHubRoute: 'MainHubRoute',
+    ProfileRoute: 'ProfileRoute',
+    SettingsRoute: 'SettingsRoute',
+    NotificationsRoute: 'NotificationsRoute',
+
+    //
+    ContactsRoute: 'ContactsRoute',
+    StoredObjectsRoute: 'StoredObjectsRoute',
+    RecoveriesRoute: 'RecoveriesRoute',
+}
+
+export const TAB_BAR_ROUTES = {
+    // MainHubRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    // ProfileRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    // OrganizationRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    // CredentialRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    // TestRoute: {header: false, tabBarIconHide: true, tabBarHide: false},
+    // WalletsRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    // NotificationsRoute: {header: false, tabBarIconHide: false, tabBarHide: false},
+    
+    // StorageRoute: {header: false, tabBarIconHide: true, tabBarHide: false},
+    // SettingsRoute: {header: false, tabBarIconHide: true, tabBarHide: true},
+    // ProfileRoute: {header: false, tabBarIconHide: true, tabBarHide: true},
+    // KeySharesRoute: {header: false, tabBarIconHide: true, tabBarHide: true},
+}
---
+++ testdata/testroute.js
@@ -0,0 +1,65 @@
+import { ROUTES } from "../config";
+
+const top_level = ROUTES.HomeRoute;
+
+const switch_routes = {
+  [ROUTES.TestRoute]: [
+    {name: ROUTES.TestRoute},
+  ],
+  [ROUTES.HomeRoute]: [
+    {name: ROUTES.HomeRoute}
+  ],
+  [ROUTES.OrganizationRoute]: [
+    {name: ROUTES.OrganizationListRoute},
+    {name: ROUTES.OrganizationViewRoute, params: {
+      organization_uuid: '6370c1f1-e047-40ff-b7dd-6fa244943f1f' //ARXsky
+    }},
+    {name: ROUTES.VerifierViewRoute, params: {
+      verifier_uuid: '2b02dc2c-4d75-43c7-b953-ec303fa4d369',
+    }},
+    // {name: ROUTES.CredentialRequestRoute, params: {
+    //   organization_uuid: 'e585bc5c-0d6d-45f4-938a-8131a42671b4',
+    //   template_uuid: 'c41c9099-d70f-4834-bd10-53f184f3e5ec', // ARXsky - Employee
+    // }},
+  ],
+  [ROUTES.CredentialRoute]: [
+    {name: ROUTES.CredentialListRoute},
+    {name: ROUTES.CredentialPresentRoute, params: {
+      verifier_uuid: '2b02dc2c-4d75-43c7-b953-ec303fa4d369',
+    }},
+    // {name: ROUTES.CredentialViewRoute, params: {
+    //   credential_uuid: '39a93352-e7e8-4eb3-ad19-351addd0fa62',
+    // }},
+  ]
+}
+
+const test_route = [
+  {
+    name: top_level,
+    state: {
+      routes: switch_routes[top_level]
+    }
+  }
+]
+
+export default test_route;
+
+
+// EXAMPLE OF TWO VALID FORMATS
+//
+// props.navigation.navigate(ROUTES.CredentialRoute, {
+//   screen: ROUTES.CredentialPresentRoute,
+//   params: {verifier_uuid: verifierUUID}
+// })
+//
+//                  OR BELOW ==
+//
+// props.navigation.navigate(ROUTES.CredentialRoute, {
+//     state: {
+//       routes: [
+//         {name: ROUTES.CredentialPresentRoute, params: {
+//           verifier_uuid: verifierUUID}
+//         }
+//       ]
+//     }
+//   })
