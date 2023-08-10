28c427ac014e47d43779894c2c9f3e79ab4af6f0
Mike Tery <miketery@gmail.com>
2023-08-10 12:41:36 -0400

    spaces to 4 from 2 updated utils functions with types

--- App.js
+++ App.js
@@ -20,34 +20,34 @@ window.Buffer = window.Buffer || require("buffer").Buffer;
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <SafeAreaView style={tw`bg-midnight h-full w-full`}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName={ROUTES.SplashRoute}
-          screenOptions={({route}) => {
-            return { headerShown: ['Test'].includes(route.name) } 
-        }}>
-          <Stack.Screen name={ROUTES.SplashRoute}>{props => 
-            <SplashScreen {...props} />}
-          </Stack.Screen>
-          <Stack.Screen 
-            name={ROUTES.LandingRoute}
-            component={LandingScreen}
-            options={{ title: 'Landing' }} />
-          <Stack.Screen name={ROUTES.VaultCreateRoute}>
-            {props => <VaultCreateScreen {...props} />}
-          </Stack.Screen>
-          {/* recover init screen */}
-          <Stack.Screen name={ROUTES.RecoverInitRoute}>
-            {props => <RecoverInitScreen {...props} />}
-          </Stack.Screen>
-          <Stack.Screen name={ROUTES.HomeRoute}>
-            {props => <HomeScreen {...props} />}
-          </Stack.Screen>
-          
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={tw`bg-midnight h-full w-full`}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName={ROUTES.SplashRoute}
+                    screenOptions={({route}) => {
+                        return { headerShown: ['Test'].includes(route.name) } 
+                }}>
+                    <Stack.Screen name={ROUTES.SplashRoute}>{props => 
+                        <SplashScreen {...props} />}
+                    </Stack.Screen>
+                    <Stack.Screen 
+                        name={ROUTES.LandingRoute}
+                        component={LandingScreen}
+                        options={{ title: 'Landing' }} />
+                    <Stack.Screen name={ROUTES.VaultCreateRoute}>
+                        {props => <VaultCreateScreen {...props} />}
+                    </Stack.Screen>
+                    {/* recover init screen */}
+                    <Stack.Screen name={ROUTES.RecoverInitRoute}>
+                        {props => <RecoverInitScreen {...props} />}
+                    </Stack.Screen>
+                    <Stack.Screen name={ROUTES.HomeRoute}>
+                        {props => <HomeScreen {...props} />}
+                    </Stack.Screen>
+                    
+                </Stack.Navigator>
+            </NavigationContainer>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
--- screens/VaultCreateScreen.js
+++ screens/VaultCreateScreen.js
@@ -7,100 +7,97 @@ import tw from '../lib/tailwind'
 
 import VaultManager from '../classes/VaultManager'
 
-import { trim_and_lower } from '../lib/utils'
+import { trim_and_lower, validateEmail } from '../lib/utils'
 import { FieldError } from '../components/'
 
 export default function VaultCreateScreen(props) {
-  const [name, setName] = useState(DEV ? 'Alice Allison' : '');
-  const [displayName, setDisplayName] = useState(DEV ? 'Ali' : '');
-  const [email, setEmail] = useState(DEV ? 'alice@arxsky.com' : '');
-  const [createLoading, setCreateLoading] = useState(false);
-  const [errors, setErrors] = useState({});
+    const [name, setName] = useState(DEV ? 'Alice Allison' : '');
+    const [displayName, setDisplayName] = useState(DEV ? 'Ali' : '');
+    const [email, setEmail] = useState(DEV ? 'alice@arxsky.com' : '');
+    const [createLoading, setCreateLoading] = useState(false);
+    const [errors, setErrors] = useState({});
 
-  const navigation = props.navigation
+    const navigation = props.navigation
 
-  const finishSubmit = (vault) => {
-    console.log('[VaultCreateScreen.finishSubmit] ' + vault.getPk())
-    Cache.setVaultPk(vault.getPk())
-    navigation.dispatch(CommonActions.reset(primary_route()))
-  }
-  const checkForm = () => {
-    const errors = {}
-    if (!displayName || trim_and_lower(displayName) === '') {
-      errors.displayName = 'Display name is required'
+    const finishSubmit = (vault) => {
+        console.log('[VaultCreateScreen.finishSubmit] ' + vault.getPk())
+        Cache.setVaultPk(vault.getPk())
+        navigation.dispatch(CommonActions.reset(primary_route()))
     }
-    // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
-    if (!String(email).toLowerCase().match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    )) {
-      errors.email = 'Valid email is required'
+    const checkForm = () => {
+        const errors = {}
+        if (!displayName || trim_and_lower(displayName) === '') {
+            errors.displayName = 'Display name is required'
+        }
+        if (!validateEmail(email)) {
+            errors.email = 'Valid email is required'
+        }
+        setErrors(errors)
+        return Object.keys(errors).length === 0
     }
-    setErrors(errors)
-    return Object.keys(errors).length === 0
-  }
-  const handleSubmit = () => {
-    console.log('[VaultCreateScreen.js] handleSubmit()')
-    if(!checkForm()) {
-      console.log('[VaultCreateScreen.js] checkForm() failed')
-      return
+    const handleSubmit = () => {
+        console.log('[VaultCreateScreen.js] handleSubmit()')
+        if(!checkForm()) {
+            console.log('[VaultCreateScreen.js] checkForm() failed')
+            return
+        }
+        setCreateLoading(true)
+        setTimeout(async () => {
+            try {
+                const vault = await VaultManager.create_vault(name, displayName, email)
+                finishSubmit(vault)
+            } catch (err) {
+                console.log(err)
+                setErrors({general: 'Something went wrong, unknown error.'})
+                setCreateLoading(false)
+            }
+        }, 250);
     }
-    setCreateLoading(true)
-    setTimeout(async () => {
-      try {
-        const vault = await VaultManager.create_vault(name, displayName, email)
-        finishSubmit(vault)
-      } catch (err) {
-        console.log(err)
-        setErrors({general: 'Something went wrong, unknown error.'})
-        setCreateLoading(false)
-      }
-    }, 250);
-  }
 
-  return (
-    <View style={[ds.mainContainerPtNoNav]}>
-      <ScrollView>
-        <Text style={ds.header}>Create Vault</Text>
-        <View style={ds.inputContainer}>
-          <Text style={ds.label}>Name</Text>
-          <TextInput
-            style={ds.input}
-            placeholder="Alice Allison"
-            placeholderTextColor="#888"
-            value={name}
-            onChangeText={setName}
-          />
-          <FieldError name="displayName" errors={errors} />
+    return (
+        <View style={[ds.mainContainerPtNoNav]}>
+            <ScrollView>
+                <Text style={ds.header}>Create Vault</Text>
+                <View style={ds.inputContainer}>
+                    <Text style={ds.label}>Name</Text>
+                    <TextInput
+                        style={ds.input}
+                        placeholder="Alice Allison"
+                        placeholderTextColor="#888"
+                        value={name}
+                        onChangeText={setName}
+                    />
+                    <FieldError name="displayName" errors={errors} />
+                </View>
+                <View style={ds.inputContainer}>
+                    <Text style={ds.label}>Display Name</Text>
+                    <TextInput
+                        style={ds.input}
+                        placeholder="Ali"
+                        placeholderTextColor="#888"
+                        value={displayName}
+                        onChangeText={setDisplayName}
+                    />
+                    <FieldError name="displayName" errors={errors} />
+                </View>
+                <View style={ds.inputContainer}>
+                    <Text style={ds.label}>Email</Text>
+                    <TextInput
+                        style={ds.input}
+                        placeholder="alice@arxsky.com"
+                        placeholderTextColor="#888"
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <FieldError name="email" errors={errors} />
+                </View>
+            </ScrollView>
+            <View style={tw`flex-grow-1`} />
+            <Pressable style={ds.createButton}
+                onPress={() => handleSubmit()}>
+                <Text style={ds.textXl}>Create & Save</Text>
+            </Pressable>
+            <View style={tw`flex-grow-1`} />
         </View>
-        <View style={ds.inputContainer}>
-          <Text style={ds.label}>Display Name</Text>
-          <TextInput
-            style={ds.input}
-            placeholder="Ali"
-            placeholderTextColor="#888"
-            value={displayName}
-            onChangeText={setDisplayName}
-          />
-          <FieldError name="displayName" errors={errors} />
-        </View>
-        <View style={ds.inputContainer}>
-          <Text style={ds.label}>Email</Text>
-          <TextInput
-            style={ds.input}
-            placeholder="alice@arxsky.com"
-            placeholderTextColor="#888"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <FieldError name="email" errors={errors} />
-        </View>
-      </ScrollView>
-      <View style={tw`flex-grow-1`} />
-      <Pressable style={ds.createButton}
-        onPress={() => handleSubmit()}>
-        <Text style={ds.textXl}>Create & Save</Text>
-      </Pressable>
-      <View style={tw`flex-grow-1`} />
-    </View>
-  )
+    )
 }
