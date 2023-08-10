e26afc9d1882c7c07eb915b5d7bc1988cbacc562
Mike Tery <miketery@gmail.com>
2023-08-10 12:41:36 -0400

    spaces to 4 from 2 updated utils functions with types

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -5,23 +5,23 @@ import { ROUTES } from '../config'
 import tw from '../lib/tailwind'
 
 export default function LandingScreen(props) {
-  return (
-    <View style={ds.landingContainer}>
-      <Text style={ds.header}>ARX</Text>
-      <View>
-        <Text style={ds.text}>Never Lose Your Keys Again</Text>
-      </View>
-      <View style={tw`flex-grow-1`} />
-      <View style={tw`justify-around mb-10 flex-col items-center`}>
-        <Pressable style={[ds.ctaButton]}
-            onPress={() => props.navigation.navigate(ROUTES.VaultCreateRoute)}>
-          <Text style={ds.buttonText}>Create Vault</Text>
-        </Pressable>
-        <Pressable style={tw`mt-10`}
-            onPress={() => props.navigation.navigate(ROUTES.RecoverInitRoute)}>
-          <Text style={ds.textSm}>Recover Vault</Text>
-        </Pressable>
-      </View>
-    </View>
-  )
+    return (
+        <View style={ds.landingContainer}>
+            <Text style={ds.header}>ARX</Text>
+            <View>
+                <Text style={ds.text}>Never Lose Your Keys Again</Text>
+            </View>
+            <View style={tw`flex-grow-1`} />
+            <View style={tw`justify-around mb-10 flex-col items-center`}>
+                <Pressable style={[ds.ctaButton]}
+                        onPress={() => props.navigation.navigate(ROUTES.VaultCreateRoute)}>
+                    <Text style={ds.buttonText}>Create Vault</Text>
+                </Pressable>
+                <Pressable style={tw`mt-10`}
+                        onPress={() => props.navigation.navigate(ROUTES.RecoverInitRoute)}>
+                    <Text style={ds.textSm}>Recover Vault</Text>
+                </Pressable>
+            </View>
+        </View>
+    )
 }
