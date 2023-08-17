87d6b4f2de0b52935737d96c71eb0f38e4a2d01c
Mike Tery <miketery@gmail.com>
2023-08-16 23:51:16 -0400

    Nav restyle

--- screens/TabNavBar.js
+++ screens/TabNavBar.js
@@ -43,60 +43,58 @@ export default function TabNavBar({ state, descriptors, navigation, possible_off
     if(TAB_BAR_ROUTES[current_route_name].tabBarHide === true)
         return null
     return (<View style={tw`absolute bottom-0 w-full`}>
-        <View style={tw`py-7 items-center w-full`}>
-            <View style={tw`flex-row bg-slate-300 justify-center mx-4 rounded-full`}>    
-                {state.routes.map((route, index) => {
-                    if(TAB_BAR_ROUTES[route.name].tabBarIconHide === true)
-                        return null
-                    const { options } = descriptors[route.key];
-                    const label =
-                        options.tabBarLabel !== undefined
-                            ? options.tabBarLabel
-                            : options.title !== undefined
-                            ? options.title
-                            : route.name;
+        <View style={tw`items-center w-full bg-slate-300 flex-row`}>
+            {state.routes.map((route, index) => {
+                if(TAB_BAR_ROUTES[route.name].tabBarIconHide === true)
+                    return null
+                const { options } = descriptors[route.key];
+                const label =
+                    options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                        ? options.title
+                        : route.name;
 
-                    const isFocused = state.index === index;
+                const isFocused = state.index === index;
 
-                    const onPress = () => {
-                        const event = navigation.emit({
-                            type: 'tabPress',
-                            target: route.key,
-                            canPreventDefault: true,
-                        });
+                const onPress = () => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
 
-                        if (!isFocused && !event.defaultPrevented) {
-                            // The `merge: true` option makes sure that the params inside the tab screen are preserved
-                            navigation.navigate({ name: route.name, merge: true });
-                        }
-                    };
+                    if (!isFocused && !event.defaultPrevented) {
+                        // The `merge: true` option makes sure that the params inside the tab screen are preserved
+                        navigation.navigate({ name: route.name, merge: true });
+                    }
+                };
 
-                    const onLongPress = () => {
-                        navigation.emit({
-                            type: 'tabLongPress',
-                            target: route.key,
-                        });
-                    };
+                const onLongPress = () => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
 
-                    return (
-                        <Pressable
-                            // accessibilityRole="button"
-                            // accessibilityState={isFocused ? { selected: true } : {}}
-                            // accessibilityLabel={options.tabBarAccessibilityLabel}
-                            testID={options.tabBarTestID}
-                            onPress={onPress}
-                            onLongPress={onLongPress}
-                            style={tw`w-15 py-3 text-center justify-center flex-row`}
-                            key={route.key}
-                        >
-                            {screenOptions(route, isFocused, options)}
-                            <Text style={[{ color: isFocused ? '#673ab7' : '#222' }, tw`text-xs`]}>
-                                {/* {label} */}
-                            </Text>
-                        </Pressable>
-                    );
-                })}
-            </View>
+                return (
+                    <Pressable
+                        // accessibilityRole="button"
+                        // accessibilityState={isFocused ? { selected: true } : {}}
+                        // accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        style={tw`w-15 py-4 text-center justify-center flex-row grow-1 border-slate-800`}
+                        key={route.key}
+                    >
+                        {screenOptions(route, isFocused, options)}
+                        <Text style={[{ color: isFocused ? '#673ab7' : '#222' }, tw`text-xs`]}>
+                            {/* {label} */}
+                        </Text>
+                    </Pressable>
+                );
+            })}
         </View>
         {(possible_offline || false) && <View style={tw`-mt-4 bg-red-700 bottom-0 relative px-2`}>
             <Text style={tw`text-xs text-slate-200`}>Error connecting to server.</Text>
