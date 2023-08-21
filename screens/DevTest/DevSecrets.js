f9c050a22ca8441f122df27be18804f7168cb7b3
Mike Tery <miketery@gmail.com>
2023-08-20 22:10:44 -0400

    added place holders for secrets screens

---
+++ screens/DevTest/DevSecrets.js
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native'
+
+export default function DevSecrets(props) {
+    const current_route = props.route.name
+    return (
+        <View style={ds.landingContainer}>
+
+            <Text style={ds.header}>Dev Secrets</Text>
+            <View>
+                <Text style={ds.text}>Route: {current_route}</Text>
+            </View>
+            <View style={tw`flex-grow-1`} />
+            <View style={tw`justify-around mb-10 flex-col items-center`}>
+
+            </View>
+        </View>
+    )
+}
