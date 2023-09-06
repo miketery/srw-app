19ef7544be00725abecb2d6cd325c95a4579008f
Mike Tery <miketery@gmail.com>
2023-09-06 15:58:51 -0400

    dev contacts work

--- screens/Dev/DevDigitalAgentScreen.js
+++ screens/Dev/DevDigitalAgentScreen.js
@@ -8,6 +8,7 @@ import ds from '../../assets/styles'
 import Cache from '../../classes/Cache'
 import DAI from '../../classes/DigitalAgentInterface'
 import { GoBackButton } from '../../components'
+import getTestContacts from '../../testdata/testContacts'
 
 function registerVault() {
     console.log('registerVault')
@@ -20,6 +21,21 @@ function amIRegistered() {
     const vm = Cache.vault_manager
     const vault = vm.current_vault
     DAI.amIRegistered(vault)
+}
+async function postMessage() {
+    console.log('postMessage')
+    const vm = Cache.vault_manager
+    // const cm = vm.contacts_manager
+    // const bob = cm.getContactByDid('Bob')
+    // const vault = vm.current_vault
+    // const msg = Message.forContact(vault, )
+    // DAI.postMessage(vault)
+    const contacts = await getTestContacts()
+    console.log(contacts)
+    // sleep 1000ms
+    console.log(typeof(contacts.alice.bob))
+    console.log(contacts['alice']['bob'].toDict())
+    console.log(contacts['bob']['alice'].toDict())
 }
 
 
@@ -34,6 +50,11 @@ export default function DevDigitalAgentScreen(props) {
             <Pressable style={[ds.button, ds.blueButton, tw`mt-4`]}
             onPress={() => amIRegistered()}>
                 <Text style={ds.buttonText}>Am I Registered</Text>
+            </Pressable>
+            
+            <Pressable style={[ds.button, ds.blueButton, tw`mt-4`]}
+            onPress={() => postMessage()}>
+                <Text style={ds.buttonText}>Post Message</Text>
             </Pressable>
         </View>
         <View style={tw`flex-grow-1`} />
---
+++ testdata/testContacts.js
@@ -0,0 +1,56 @@
+import { ContactState } from '../classes/Contact';
+import Vault from '../classes/Vault';
+import Contact from '../classes/Contact';
+import { test_vaults } from './testVaults.js';
+
+import { encryptionKeyFromWords } from '../lib/utils';
+
+const words = {
+    'alice': {
+        'bob': 'stereo address seven like end observe garage obvious pen post hospital bunker',
+        'charlotte': 'solid wedding silent grab used refuse blouse aim tape grape drastic weather',
+    },
+    'bob': {
+        'alice': 'inner beauty setup air cream demise radar arrow game oppose call replace',
+        'charlotte': 'receive black rally tragic slight sand hero rapid veteran basic journey coral',
+    },
+    'charlotte': {
+        'alice': 'divorce bomb game rib furnace reduce nerve eagle winner smoke october problem',
+        'bob': 'evolve stay spider noise bubble imitate adjust logic inside start object lend',
+    }
+}
+
+const participants = Object.keys(words)
+
+const vaults = Object.fromEntries(test_vaults.map((vault) => {
+    return Vault.fromDict(vault)
+}).map((vault) => [vault.name, vault]))
+
+
+async function getTestContacts() {
+    const contacts = {}
+    for(let i = 0; i < participants.length; i++) {
+        const name = participants[i]
+        const my_vault = vaults[name]
+        const their_names = Object.keys(words[name])
+        contacts[name] = {}
+        for(let j = 0; j < their_names.length; j++) {
+            const their_name = their_names[j]
+            const my_words = words[name][their_name]
+            const their_vault = vaults[their_name]
+            const their_words = words[their_name][name]
+            const theirContactKeyPair = encryptionKeyFromWords(their_words)
+            const myContactKeyPair = encryptionKeyFromWords(my_words)
+            const contact = await Contact.create(
+                my_vault.pk, their_vault.did, their_name,
+                their_vault.public_key, their_vault.verify_key,
+                theirContactKeyPair.publicKey, '', ContactState.ACCEPTED)
+            contact.private_key = myContactKeyPair.secretKey
+            contact.public_key = myContactKeyPair.publicKey
+            contacts[name][their_name] = contact
+        }
+    }
+    return contacts
+}
+
+export default getTestContacts
